338bc58d52d1c3d333b7d9d0097003625f7408f4
Nakidai <plaza521@inbox.ru>
2024-03-16 14:54:16 +0300

    Now strinstruction is switch-case

--- src/luts.c
+++ src/luts.c
@@ -1,41 +1,5 @@
 #include "types.h"
 
-static const s8 *instructions[128] =
-{
-  /*    0       1       2       3       4       5       6       7 */
-    "NOP ", "LRA ", "SRA ", "CALL", "RET ", "JMP ", "JMPP", "JMPZ",
-  /*    8       9      10      11      12      13      14      15 */
-    "JMPN", "JMPC", "CCF ", "LRP ", "CCP ", "CRP ", "PUSH", "POP ",
-  /*   16      17      18      19      20      21      22      23 */
-    "AND ", "OR  ", "XOR ", "NOT ", "LSC ", "RSC ", "CMP ", "CMPU",
-  /*   24      25      26      27      28      29      30      31 */
-    "UNKN", "UNKN", "UNKN", "UNKN", "UNKN", "UNKN", "UNKN", "UNKN",
-  /*   32      33      34      35      36      37      38      39 */
-    "ADC ", "SBC ", "INC ", "DEC ", "ABS ", "MUL ", "DIV ", "MOD ",
-  /*   40      41      42      43      44      45      46      47 */
-    "TSE ", "TCE ", "ADD ", "SUB ", "RPL ", "UNKN", "UNKN", "UNKN",
-  /*   48      49      50      51      52      53      54      55 */
-    "UI  ", "UO  ", "UOC ", "UOCR", "RPL ", "UNKN", "UNKN", "UNKN",
-  /*   56      57      58      59      60      61      62      63 */
-    "UNKN", "UNKN", "UNKN", "UNKN", "UNKN", "UNKN", "UNKN", "UNKN",
-  /*   64      65      66      67      68      69      70      71 */
-    "UNKN", "UNKN", "UNKN", "UNKN", "UNKN", "UNKN", "UNKN", "UNKN",
-  /*   72      73      74      75      76      77      78      79 */
-    "UNKN", "UNKN", "UNKN", "UNKN", "UNKN", "UNKN", "UNKN", "UNKN",
-  /*   80      81      82      83      84      85      86      87 */
-    "UNKN", "UNKN", "UNKN", "UNKN", "UNKN", "UNKN", "UNKN", "UNKN",
-  /*   88      89      90      91      92      93      94      95 */
-    "UNKN", "UNKN", "UNKN", "UNKN", "UNKN", "UNKN", "UNKN", "UNKN",
-  /*   96      97      98      99     100     101     192     193 */
-    "UNKN", "UNKN", "UNKN", "UNKN", "UNKN", "UNKN", "UNKN", "UNKN",
-  /*  104     105     106     107     108     109     110     111 */
-    "UNKN", "UNKN", "UNKN", "UNKN", "UNKN", "UNKN", "UNKN", "UNKN",
-  /*  112     113     114     115     116     117     118     119 */
-    "PRW ", "PRR ", "UNKN", "UNKN", "UNKN", "UNKN", "UNKN", "UNKN",
-  /*  120     121     122     123     124     125     126     127 */
-    "UNKN", "UNKN", "UNKN", "UNKN", "UNKN", "UNKN", "INT ", "HALT",
-};
-
 static const u8 sins[256] =
 {
     128, 131, 135, 139, 143, 147, 151, 155,
@@ -146,7 +110,59 @@ static const u8 rpls[] =
 
 const s8 *emulator_strinstruction(u8 instruction)
 {
-    return instructions[instruction];
+    switch (instruction)
+    {
+    case 0:   return "NOP ";
+    case 1:   return "LRA ";
+    case 2:   return "SRA ";
+    case 3:   return "CALL";
+    case 4:   return "RET ";
+    case 5:   return "JMP ";
+    case 6:   return "JMPP";
+    case 7:   return "JMPZ";
+    case 8:   return "JMPN";
+    case 9:   return "JMPC";
+    case 10:  return "CCF ";
+    case 11:  return "LRP ";
+    case 12:  return "CCP ";
+    case 13:  return "CRP ";
+    case 14:  return "PUSH";
+    case 15:  return "POP ";
+    case 16:  return "AND ";
+    case 17:  return "OR  ";
+    case 18:  return "XOR ";
+    case 19:  return "NOT ";
+    case 20:  return "LSC ";
+    case 21:  return "RSC ";
+    case 22:  return "CMP ";
+    case 23:  return "CMPU";
+    /* ... */
+    case 32:  return "ADC ";
+    case 33:  return "SBC ";
+    case 34:  return "INC ";
+    case 35:  return "DEC ";
+    case 36:  return "ABS ";
+    case 37:  return "MUL ";
+    case 38:  return "DIV ";
+    case 39:  return "MOD ";
+    case 40:  return "TSE ";
+    case 41:  return "TCE ";
+    case 42:  return "ADD ";
+    case 43:  return "SUB ";
+    case 44:  return "RPL ";
+    /* ... */
+    case 48:  return "UI  ";
+    case 49:  return "UO  ";
+    case 50:  return "UOC ";
+    case 51:  return "UOCR";
+    case 52:  return "RPL ";
+    /* ... */
+    case 112: return "PRW ";
+    case 113: return "PRR ";
+    case 126: return "INT ";
+    case 127: return "HALT";
+    }
+    return "UNKN";
 }
 
 u8 emulator_lut_sin(u8 n)
